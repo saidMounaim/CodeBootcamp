ae757c24fee2a310cfc5a4d5b53c146998beaa0e
Said Mounaim <saidmounaim00@gmail.com>
2021-04-19 18:46:19 +0000

    Add User Login

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/User.js';
 import generateToken from '../utils/generateToken.js';
+import bcrypt from 'bcryptjs';
 
 //@DESC Register
 //@ROUTE /api/v1/auth/register
@@ -19,4 +20,34 @@ const register = asyncHandler(async (req, res) => {
 	res.status(201).json({ success: true, token });
 });
 
-export { register };
+//@DESC Login
+//@ROUTE /api/v1/auth/login
+//@METHOD POST
+const login = asyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+
+	if (!email || !password) {
+		res.status(401);
+		throw new Error(`Please provide an email and password`);
+	}
+
+	const user = await User.findOne({ email: email }).select('+password');
+
+	if (!user) {
+		res.status(401);
+		throw new Error('Invalid credentials');
+	}
+
+	const passwordInvalid = bcrypt.compareSync(password, user.password);
+
+	if (!passwordInvalid) {
+		res.status(401);
+		throw new Error('Invalid password');
+	}
+
+	const token = generateToken(user._id);
+
+	res.status(201).json({ success: true, token });
+});
+
+export { register, login };
--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { register } from '../controllers/AuthController.js';
+import { register, login } from '../controllers/AuthController.js';
 
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/login', login);
 
 export default router;
