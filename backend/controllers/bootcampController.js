63d18579c3b1cac6f672793e4823cd504b6be9a4
Said Mounaim <saidmounaim00@gmail.com>
2021-04-22 21:23:50 +0000

    Bootcamp Ownership

--- backend/controllers/bootcampController.js
+++ backend/controllers/bootcampController.js
@@ -46,16 +46,24 @@ export const createBootcamps = asyncHandler(async (req, res) => {
 // @ROUTE /api/v1/bootcamps/:id
 export const updateBootcamps = asyncHandler(async (req, res) => {
 	const { id } = req.params;
-	if (id) {
-		const bootcamp = await Bootcamp.findByIdAndUpdate(id, req.body, {
-			new: true,
-			runValidators: true,
-		});
-		res.status(200).json({ success: true, data: bootcamp });
-	} else {
+	let bootcamp = await Bootcamp.findById(id);
+
+	if (!bootcamp) {
 		res.status(404);
 		throw new Error(`Bootcamp Not Found`);
 	}
+
+	if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+		res.status(401);
+		throw new Error('user role not authorize to update this bootcamp');
+	}
+
+	bootcamp = await Bootcamp.findByIdAndUpdate(id, req.body, {
+		new: true,
+		runValidators: true,
+	});
+
+	res.status(200).json({ success: true, data: bootcamp });
 });
 
 // @DESC Delete Bootcamps
@@ -63,14 +71,21 @@ export const updateBootcamps = asyncHandler(async (req, res) => {
 // @ROUTE /api/v1/bootcamps
 export const deleteBootcamps = asyncHandler(async (req, res) => {
 	const { id } = req.params;
+
 	const bootcamp = await Bootcamp.findById(id);
 	bootcamp.remove();
-	if (id) {
-		res.status(200).json({ success: true, data: {} });
-	} else {
+
+	if (!bootcamp) {
 		res.status(404);
 		throw new Error(`Bootcamp Not Found`);
 	}
+
+	if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+		res.status(401);
+		throw new Error('user role not authorize to delete this bootcamp');
+	}
+
+	res.status(200).json({ success: true, data: {} });
 });
 
 // @DESC Get Bootcamps within a radius
@@ -108,6 +123,11 @@ export const bootcampUploadPhoto = asyncHandler(async (req, res) => {
 	if (!bootcamp) {
 		res.status(404);
 		throw new Error('Bootcamp Not Found');
+	}
+
+	if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+		res.status(401);
+		throw new Error('user role not authorize to upload photo');
 	}
 
 	if (!req.files) {
